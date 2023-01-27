51a16ae87feed9df9b49f341eb4501bd8a7467c6
KDLSA <alexprogd87@gmail.com>
2023-01-27 11:46:23 -0500

    update 0.32

---
+++ java/novedades.js
@@ -0,0 +1,7 @@
+function Siguiente(){
+    document.getElementById("Contenido2").style.display = "block"
+}
+
+function Ocultar(){
+    document.getElementById("Contenido").style.display = "none"
+}
